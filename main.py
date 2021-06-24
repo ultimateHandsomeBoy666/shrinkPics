3327fe4e3ac7d6e0c88854324174f7af419a534a
guozhexuan <gordon.guo@royole.com>
2021-06-24 09:54:22 +0800

    add more print

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import os
 
 
 tinify.key = "j2h729DzSXz5ZfkTJ5Lq4dXZl8dcKSbQ"
+total_old_size = 0
+total_new_size = 0
+
+
+def get_file_size_KB(file):
+    file_size = os.path.getsize(file) / 1024.0
+    return round(file_size, 2)
+
+
+def get_file_size_MB(file):
+    file_size = os.path.getsize(file) / 1024.0 / 1024.0
+    return round(file_size, 2)
 
 
 def colored(r, g, b, text):
@@ -17,12 +29,23 @@ def compress(path):
         if os.path.isdir(file):
             compress(sub_file)
         else:
-            print("current file: " + sub_file)
-            if is_pic(file):
-                print(colored(255, 25, 25, "last file is pic, tinify it..."))
+            old_file_size = get_file_size_KB(sub_file)
+            total_old_size += old_file_size
+            if is_pic(sub_file):
+                print(colored(255, 25, 25,
+                              "current file: {0} is pic, size: {1}, tinify it...".format(sub_file, str(old_file_size))))
+
                 source = tinify.from_file(sub_file)
                 source.to_file(sub_file)
-                print(colored(255, 25, 25, "last file is pic, tinify done!"))
+
+                new_file_size = get_file_size_KB(sub_file)
+                total_new_size += new_file_size
+
+                print(colored(255, 25, 25, "tinify done! now the pic size: {0}, shrunk by {1}%".format(
+                    str(new_file_size), str(100 - 100 * new_file_size / old_file_size))))
+            else:
+                total_new_size += old_file_size
+                print("current file: {0} is pic, size: {1}".format(sub_file, str(old_file_size)))
 
 
 def is_pic(file):
@@ -35,3 +58,10 @@ def is_pic(file):
 if __name__ == '__main__':
     print("current path: " + os.path.curdir)
     compress(os.path.curdir)
+    if total_new_size == total_old_size:
+        print("compress done! But no pics were found in the directory.")
+    else:
+        print("compress done! All pics shrunk from {0}KB({1}MB) to {2}KB({3}MB), shrunk by {4}%.".format(
+            total_old_size, total_old_size / 1024, total_new_size, total_new_size / 1024,
+            str(100 - 100 * total_new_size / total_old_size)))
+
